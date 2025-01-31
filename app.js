3f4348f37472349a54c4d130f96812ff6cd078f9
TheJoaoCastro <theferenciado@gmail.com>
2025-01-31 10:33:29 -0300

    feat: acrescentar lógica de sorteio

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = []
+
+function adicionarAmigo() {
+
+    let nome = document.getElementById('amigo').value
+
+    if (nome == '') {
+        alert('Campo não preenchido!')
+    } else {
+        nomes.push(nome)
+        document.getElementById('listaAmigos').innerHTML += `<li>${nome}</li>`
+    }
+}
+
+function sortearAmigo() {
+    numeroSorteado = parseInt(Math.random() * nomes.length)
+    document.getElementById('listaAmigos').innerHTML = ''
+    document.getElementById('resultado').innerHTML = `A pessoa sorteada: ${nomes[numeroSorteado]}`
+}
